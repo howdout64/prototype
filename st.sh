90cc2a5686b5e7f8e09995f21de463624cc63d33
howdout64 <115226861+howdout64@users.noreply.github.com>
2022-12-27 15:59:04 +0300

    Update st.sh

--- st.sh
+++ st.sh
@@ -1,7 +1,9 @@
 function shsh {
     printf "\e[94mroot@\e[34mprototype \e[96m~#\e[97m "
     read -r command
-    if [[ $command == "stop" ]] then
+    if [ "$command" == "stop" ]; then
+     pkill java
+    elif [ "$command" == "end" ]; then
      pkill java
     else
      ./proot -r . -S . -b . /bin/bash -c "$command"
