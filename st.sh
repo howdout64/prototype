b09511490b9612db4a5d72c0ada9d9270f40bc43
howdout64 <115226861+howdout64@users.noreply.github.com>
2022-12-27 14:24:21 +0300

    Update st.sh

--- st.sh
+++ st.sh
@@ -1,7 +1,7 @@
 function shsh {
     printf "\e[94mroot@\e[34mprototype \e[96m~#\e[97m "
     read -r command
-    ./proot -r . -S . -b /proc -0 /bin/bash -c "$command"
+    ./proot -r . -S . -b . /bin/bash -c "$command"
     shsh
 }
 
