03204c02474be2c682370f6d969f487c8cbffff1
howdout64 <115226861+howdout64@users.noreply.github.com>
2022-12-27 15:51:09 +0300

    Delete utils.sh

--- utils.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt-get update ; apt-get install locales neofetch curl wget git make -y
-locale-gen en_US.UTF-8
-apt-get install python3 -y
-apt-get install systemd -y
-rm /bin/systemctl
-curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
-chmod 775 /bin/systemctl
